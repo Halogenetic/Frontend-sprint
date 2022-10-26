dd40d24d2f8650309d03f5be3059327d7b6aaab3
Halogenetic <benjamin.akh@gmail.com>
2022-10-26 11:58:14 +0200

    second commit

---
+++ Whack A Mole/script.js
@@ -0,0 +1,43 @@
+let seconds = 1000
+let sco = 0
+
+
+const startGame = () => {
+    let randomnbr = Math.floor(Math.random() * 12)
+    console.log(randomnbr)
+    let moles = document.getElementsByClassName("mole")
+    moles[randomnbr].style.visibility = "visible";
+    stopi = setTimeout(startGame, seconds)
+    if (sco>=100) {
+        clearInterval(stopi);
+        let holes = document.querySelectorAll(".hole")
+        holes.forEach((hole) => {        
+            hole.style.visibility = "hidden"; })
+        let moles = document.querySelectorAll(".mole")
+        moles.forEach((mole) => {        
+        mole.style.visibility = "hidden"; })
+        let guacam = document.querySelector(".viewer")
+            guacam.style.backgroundimage = 'url(./images/guaca.jpg)';
+
+        // alert("GUACAMOLE !");
+    }
+}
+
+timerStart = document.querySelector("button")
+timerStart.addEventListener("click", () => {
+    startGame()
+});
+
+
+
+moles = document.querySelectorAll(".mole")
+moles.forEach((mole) => {
+    mole.addEventListener('click', (event) => {
+    event.target.style.visibility = "hidden";
+    score = document.querySelector(".score")
+    sco++
+    seconds -= 10
+    score.innerHTML = sco
+    })
+}
+)
--- script.js
+++ script.js
@@ -1,38 +1,74 @@
-let seconds = 1000
-let sco = 0
+const calcTime = (city, offset) => {
 
-
-const startGame = () => {
-    let randomnbr = Math.floor(Math.random() * 12)
-    console.log(randomnbr)
-    let moles = document.getElementsByClassName("mole")
-    moles[randomnbr].style.visibility = "visible";
-    stopi = setTimeout(startGame, seconds)
-    if (sco==3) {
-        clearInterval(stopi);
-        alert("GUACAMOLE !");
-        let holes = document.getElementsByClassName("hole")
-        holes.style.visibility = "hidden";
-        let guaca = document.getElementsByClassName("viewer")
-        guaca.style.background = "-moz-element()";
-    }
+    // create Date object for current location
+    const d = new Date();
+   
+    // get UTC time in msec
+    const utc = d.getTime();
+   
+    // create new Date object for different city
+    // using supplied offset
+    const nd = new Date(utc + (3600000*offset));
+   
+    // document.write time as a string
+    document.write("The local time in " + city + " is " + nd.toLocaleString()+"<br>")
 }
 
-timerStart = document.querySelector("button")
-timerStart.addEventListener("click", () => {
-    startGame()
-});
+calcTime("Brussels (Belgium)", 0)
+calcTime("Anchorage (USA)", -10)
+calcTime("Reykjavik (Iceland)", -2)
+calcTime("Saint-Petersburg (Russia)", +1)
+
+
+// To set two dates to two constiables
+const thedate = new Date("09/22/1991");
+const today = new Date();
+  
+// To calculate the time difference of two dates
+const Difference_In_Time = today.getTime() - thedate.getTime();
+  
+// To calculate the no. of days between two dates
+const Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+  
+//To display the final no. of days (result)
+document.write("<br> Total number of days between dates  <br>"
+               + thedate + " and <br>" 
+               + today + " is: <br> " 
+               + Difference_In_Days);
+
+
+const daysDifference = (thedate, today) => {
+    const Difference_In_Time = today.getTime() - thedate.getTime();
+    const Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    document.write("<br> <br> Total number of days between dates  <br>"
+               + thedate + " and <br>" 
+               + today + " is: <br> " 
+               + Difference_In_Days);}
+            
+daysDifference (new Date("09/11/2001"), new Date())
 
 
 
-moles = document.querySelectorAll(".mole")
-moles.forEach((mole) => {
-    mole.addEventListener('click', (event) => {
-    event.target.style.visibility = "hidden";
-    score = document.querySelector(".score")
-    sco++
-    seconds -= 10
-    score.innerHTML = sco
-    })
-}
-)
+newdiv = document.createElement("div")
+document.body.appendChild(newdiv)
+newinput = document.createElement("input")
+newdiv.appendChild(newinput)
+newinput.type="number";
+newp = document.createElement("p")
+newdiv.appendChild(newp)
+
+
+newinput.addEventListener('keyup', (event) => {
+    const today = new Date();
+    const tdtime = today.getTime();
+    const hrtime = ((newinput.value)*(60*60*1000));
+    const tdhr = new Date ((tdtime + hrtime));
+
+    newp.textContent = ("In "+newinput.value+" hours, it will be "+tdhr.toLocaleString())
+
+})
+
+
+
+
+
